678c5c9183e57901e3da9d769869004870546ff0
hp <purvravani06@gmail.com>
2023-01-04 10:18:11 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:file/src/interface/file.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
-import 'package:path/path.dart';
+
 
 void main(){
   runApp(MaterialApp(home:Home() ,));
@@ -15,12 +16,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  void initState() {
+    super.initState();
+    get();
+  }
+  get() async {
+    File file = await DefaultCacheManager().getSingleFile("url");
+  }
+
   List<String> imgList = [
     "assets/images/sagar.jpg",
     "assets/images/sagar.jpg",
     "assets/images/sagar.jpg",
     "assets/images/sagar.jpg",
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,11 +54,11 @@ class _HomeState extends State<Home> {
                     floatingActionButton: FloatingActionButton(
                         onPressed: () async {
                           int location = WallpaperManager.BOTH_SCREEN;
-                          var file = await DefaultCacheManager();
-                              //.getSingleFile();
-                          // bool result =
-                          // await WallpaperManager.setWallpaperFromFile(
-                          //     file.path, location); //provide image path
+                          var file = await DefaultCacheManager().getSingleFile(AutofillHints.photo);
+
+                          bool result =
+                          await WallpaperManager.setWallpaperFromFile(
+                              file.path, location); //provide image path
                         },
                         child: Icon(Icons.download)),
                   );
